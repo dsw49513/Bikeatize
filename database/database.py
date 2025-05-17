3081be57b79ac6ac28acb091639e2e4ce9713ea9
dsw49513 <dsw49513@student.dsw.edu.pl>
2025-05-17 20:53:57 +0200

    Polaczono FastAPI z MySQL, dodano user

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+asyncmy://root:bikeatize2001$%%@localhost/bikeatize_db"
+DATABASE_URL = "mysql+asyncmy://root:bikeatize2001$%@localhost/bikeatize_db"
 
 engine = create_async_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
