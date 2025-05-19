bff7227eb0da4cf9103441c37eae40c0faf0f4b7
dsw49513 <dsw49513@student.dsw.edu.pl>
2025-05-19 16:03:31 +0200

    Dynamiczna konfiguracja bazy danych

--- database/database.py
+++ database/database.py
@@ -1,7 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+asyncmy://root:bikeatize2001$%@localhost/bikeatize_db"
+# Ładowanie zmiennych środowiskowych
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("Brak zmiennej DATABASE_URL w .env!")  # Zapobiega uruchomieniu aplikacji bez konfiguracji
 
 engine = create_async_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
