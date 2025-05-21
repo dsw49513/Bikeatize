5d8ad55a29b938635439b6e467f6ec5ff5b8b67b
dsw49513 <dsw49513@student.dsw.edu.pl>
2025-05-21 22:36:31 +0200

    Wstępna implementacja algorytmu Haversine do obliczania dystansu i do dalszej pracy nad przebyta trasa

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from database.database import init_db  # Import funkcji inicjalizującej bazę danych
 from backend.routes.geolocation import router as geolocation_router
 from backend.auth import router as auth_router  # Router autoryzacji
+from backend.routes.distance import router as distance_router
 
 # Tworzenie aplikacji FastAPI
 app = FastAPI()
@@ -14,6 +15,7 @@ async def startup_event():
 # Rejestracja routerów
 app.include_router(geolocation_router)  # Endpointy lokalizacji
 app.include_router(auth_router)  # Endpointy związane z autoryzacją
+app.include_router(distance_router)  # Endpointy do zliczania kilometrów
 
 # Endpoint testowy, aby sprawdzić, czy API działa
 @app.get("/")
--- database/database.py
+++ database/database.py
@@ -24,3 +24,8 @@ Base = declarative_base()
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+# Funkcja 'get_db()' do pobierania sesji bazy danych
+async def get_db():
+    async with SessionLocal() as db:
+        yield db
