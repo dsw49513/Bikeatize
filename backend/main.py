77a5ff2a9c4e029e16ded2615bd3ed01161a2976
Paweł Pawiłojć <pawilojc@gmail.com>
2025-05-24 14:32:36 +0200

    Add CORS middleware to allow frontend access

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
-from database.database import init_db  # Import funkcji inicjalizującej bazę danych
+# Import funkcji inicjalizującej bazę danych
+from database.database import init_db
 from backend.routes.geolocation import router as geolocation_router
 from backend.auth import router as auth_router  # Router autoryzacji
 from backend.routes.distance import router as distance_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Tworzenie aplikacji FastAPI
 app = FastAPI()
 
 # Wywołanie inicjalizacji bazy danych przy starcie serwera
+
+
 @app.on_event("startup")
 async def startup_event():
     await init_db()
@@ -18,6 +23,17 @@ app.include_router(auth_router)  # Endpointy związane z autoryzacją
 app.include_router(distance_router)  # Endpointy do zliczania kilometrów
 
 # Endpoint testowy, aby sprawdzić, czy API działa
+
+
 @app.get("/")
 async def root():
     return {"message": "API is running!"}
+
+# Otwarcie dostępu dla frontendu
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adres frontendu
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
